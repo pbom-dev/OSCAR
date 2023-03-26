2c76ccae346bcaaa85692e3753ea7fe4b82b49f3
Maximiliano Ozernickz <maxoozer@gmail.com>
2023-03-26 16:56:16 +0300

    Delete npm-package directory

--- npm-package/index.js
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-
-function getOscarMatrix() {
-  const data = fs.readFileSync("pbom_data/matrix.json", "utf8");
-  return JSON.parse(data);
-}
-
-export default getOscarMatrix;
